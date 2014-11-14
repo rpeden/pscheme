dd5c663acf9b25348c78ca82afa245afd671d12b
Ryan Peden <ryan04@gmail.com>
2014-11-14 06:22:24 -0500

    commented out description area for now

--- PScm.pm
+++ PScm.pm
@@ -13,24 +13,33 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(ReadEvalPrint);
 
-=head1 NAME
-
-PScm - A fun schemish language written in Perl
-
-=head1 SYNOPSIS
-
-  use PScm;
-  ReadEvalPrint($in_filehandle[, $out_filehandle]);
-
- =head1 DESCRIPTION
-
- A fun toy lisp interpreter. Using as practice because
- I Want to implement an evem better one. 
-
- =cut
+#=head1 NAME
+#
+#PScm - A fun schemish language written in Perl
+#
+#=head1 SYNOPSIS
+#
+#  use PScm;
+#  ReadEvalPrint($in_filehandle[, $out_filehandle]);
+#
+# =head1 DESCRIPTION
+#
+# A fun toy lisp interpreter. Using as practice because
+# I Want to implement an evem better one. 
+#
+# =cut
 
 our $GlobalEnv = new PScm::Env(
 	'*' => new PScm::Primitive::Multiply(),
 	'-' => new PScm::Primitive::Subtract(),
 	if  => new PScm::SpecialForm::If(),
 );
+
+sub ReadEvalPrint{
+	my ($infh, $outfh) = @_;
+	my $reader = new PScm::Read($infh);
+	while (defined(my $expr = $reader->Read)){
+		my $result = $expr->Eval();
+		$result->Print($outfh);
+	}
+}
