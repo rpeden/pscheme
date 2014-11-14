b37a0b0fdfb9bced2e37da512557edebe001754b
Ryan Peden <ryan04@gmail.com>
2014-11-13 20:42:39 -0500

    added description and global environment

--- PScm.pm
+++ PScm.pm
@@ -12,3 +12,25 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(ReadEvalPrint);
+
+=head1 NAME
+
+PScm - A fun schemish language written in Perl
+
+=head1 SYNOPSIS
+
+  use PScm;
+  ReadEvalPrint($in_filehandle[, $out_filehandle]);
+
+ =head1 DESCRIPTION
+
+ A fun toy lisp interpreter. Using as practice because
+ I Want to implement an evem better one. 
+
+ =cut
+
+our $GlobalEnv = new PScm::Env(
+	'*' => new PScm::Primitive::Multiply(),
+	'-' => new PScm::Primitive::Subtract(),
+	if  => new PScm::SpecialForm::If(),
+);
