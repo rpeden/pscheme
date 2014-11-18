3a11bbb25ef7b8d726d717fbb04642726b6625b2
Ryan Peden <ryan04@gmail.com>
2014-11-18 11:19:32 -0500

    Added Token::Open and Token::Close classes

--- Token.pm
+++ Token.pm
@@ -6,3 +6,17 @@ use base qw(PScm);
 
 sub is_open_token { 0 }
 sub is_close_token { 0 }
+
+########################
+package PScm::Token::Open;
+
+use base qw(PScm::Token);
+
+sub is_open_token { 1 }
+
+########################
+package PScm::Token::Close;
+
+use base qw(PScm::Token);
+
+sub is_close_token { 1 }
